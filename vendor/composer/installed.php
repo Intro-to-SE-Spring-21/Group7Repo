31ba0b9d91e8dd68f5cdd5733e0878e76f243ba5
David Purvis <davidpurvis647@gmail.com>
2021-04-18 19:46:48 -0500

    Added PHPstan analyze tool

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '046869c19060e2e19c9bd307f8b5d5027f61655e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '046869c19060e2e19c9bd307f8b5d5027f61655e',
+    ),
+    'phpstan/phpstan' => 
+    array (
+      'pretty_version' => '0.12.83',
+      'version' => '0.12.83.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4a967cec6efb46b500dd6d768657336a3ffe699f',
+    ),
+  ),
+);
